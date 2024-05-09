46f5d5b29278480b44ba221456cd66f3727e0ab4
Nicolas <nicolas.bouffanais.1999@gmail.com>
2024-05-08 21:34:02 -0400

    3 pages working

--- src/main.go
+++ src/main.go
@@ -43,6 +43,9 @@ func main() {
 	ctx2, cancel2 := chromedp.NewContext(ctx)
 	defer cancel2()
 
+	ctx3, cancel3 := chromedp.NewContext(ctx)
+	defer cancel3()
+
 	bot, err := tb.NewBot(tb.Settings{
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 		Token:  token,
@@ -64,32 +67,51 @@ func main() {
 		}()
 	})
 
-	bot.Handle("/pagina2", func(m *tb.Message) {
+	bot.Handle("/Dafiti", func(m *tb.Message) {
 		go Dafiti(ctx2, bot, m, "https://pkg.go.dev/gopkg.in/telebot.v3@v3.2.1")
+	})
+
+	bot.Handle("/SBS", func(m *tb.Message) {
+		go Dafiti(ctx3, bot, m, "https://www.youtube.com/")
 	})
 
 	log.Println("Bot is running. Press CTRL+C to exit.")
 	bot.Start()
 }
 
-// func handlePage(bot *tb.Bot, m *tb.Message, url string) {
-
-// 	// Ejecuta la primera goroutine para la primera página
-// 	go func() {
-// 		if err := navigateAndSendMessage(ctx1, bot, m, url); err != nil {
-// 			log.Println("Error en la goroutine 1:", err)
-// 		}
-// 	}()
-
-// 	// Ejecuta la segunda goroutine para la segunda página
-// 	go func() {
-// 		if err := Dafiti(ctx2, bot, m, url); err != nil {
-// 			log.Println("Error cargando Dafiti:", err)
-// 		}
-// 	}()
-// }
-
 func Dafiti(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
+
+	// Crea un navegador Chrome
+	ctx, cancel := chromedp.NewContext(ctx)
+	defer cancel()
+
+	var buf []byte
+
+	// Navega a la página
+	task := chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.CaptureScreenshot(&buf),
+	}
+
+	err := chromedp.Run(ctx, task)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Crear un lector para el buffer de bytes
+	reader := bytes.NewReader(buf)
+
+	// Enviar la imagen como un mensaje de foto
+	photo := &tb.Photo{File: tb.FromReader(reader)}
+	_, err = bot.Send(m.Chat, photo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func SBS(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
 	// Crea un navegador Chrome
 	ctx, cancel := chromedp.NewContext(ctx)
 	defer cancel()
