79bad68e1b88383a1cb9e18d26628f07c89194e6
Nicolas Ignacio Bouffanais Lay <83379327+N-Ignacio-Bouffanais@users.noreply.github.com>
2024-05-10 15:06:46 -0400

    Update main.go from notebook
    changes on urls from notebook sistemo

--- src/main.go
+++ src/main.go
@@ -21,6 +21,8 @@ func main() {
 	token := os.Getenv("BOT_TOKEN")
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
+	admin_user := os.Getenv("ADMIN_USER")
+	admin_pass := os.Getenv("ADMIN_PASS")
 
 	// Verificar si las variables de entorno están definidas
 	if token == "" || user == "" || password == "" {
@@ -28,10 +30,12 @@ func main() {
 		return
 	}
 
-	// Utilizar las variables de entorno
-	// fmt.Println("TOKEN:", token)
-	// fmt.Println("USER:", user)
-	// fmt.Println("PASSWORD:", password)
+	//Utilizar las variables de entorno
+	fmt.Println("TOKEN:", token)
+	fmt.Println("USER:", user)
+	fmt.Println("PASSWORD:", password)
+	fmt.Println("ADMIN_USER:", admin_user)
+	fmt.Println("Admin_pass:", admin_pass)
 
 	// Crea un contexto padre para el navegador
 	ctx, cancel := chromedp.NewContext(context.Background())
@@ -44,8 +48,8 @@ func main() {
 	ctx2, cancel2 := chromedp.NewContext(ctx)
 	defer cancel2()
 
-	ctx3, cancel3 := chromedp.NewContext(ctx)
-	defer cancel3()
+	// ctx3, cancel3 := chromedp.NewContext(ctx)
+	// defer cancel3()
 
 	// ctx4, cancel4 := chromedp.NewContext(ctx)
 	// defer cancel4()
@@ -60,10 +64,10 @@ func main() {
 
 	// Mapa para almacenar los comandos y sus descripciones
 	commands := map[string]string{
-		"/Dafiti":  "Abre el sitio web de Dafiti.",
-		"/SBS":     "Abre el sitio web de SBS.",
-		"/flr":     "Abre el sitio de FLR.",
-		"flr_info": "Retorna información delicada del sitio",
+		"/Tiendas":  "Abre el sitio web de Tiendas.",
+		"/SBS":      "Abre el sitio web de SBS.",
+		"/flr":      "Abre el sitio de FLR.",
+		"/flr_info": "Retorna información delicada del sitio",
 	}
 
 	// Manejador de comando para mostrar todos los comandos disponibles
@@ -80,16 +84,16 @@ func main() {
 	})
 
 	bot.Handle("/flr", func(m *tb.Message) {
-		go flr(ctx1, bot, m, "https://www.chess.com/login", user, password)
+		go flr(ctx1, bot, m, "http://10.115.43.118:3008/il/grafana/login", user, password)
 	})
 
-	bot.Handle("/Dafiti", func(m *tb.Message) {
-		go Dafiti(ctx2, bot, m, "https://pkg.go.dev/gopkg.in/telebot.v3@v3.2.1")
+	bot.Handle("/Tiendas", func(m *tb.Message) {
+		go Tiendas(ctx2, bot, m, "http://10.115.43.82:3002/login", admin_user, admin_pass)
 	})
 
-	bot.Handle("/SBS", func(m *tb.Message) {
-		go Dafiti(ctx3, bot, m, "https://www.youtube.com/")
-	})
+	// bot.Handle("/SBS", func(m *tb.Message) {
+	// 	go Tiendas(ctx3, bot, m, "https://www.youtube.com/")
+	// })
 
 	// bot.Handle("/flr_info", func(m *tb.Message) {
 	// 	go flr_info(ctx4, bot, m, "https://www.chess.com/login", user, password)
@@ -106,10 +110,12 @@ func flr(ctx context.Context, bot *tb.Bot, m *tb.Message, url string, user strin
 	// Navega a la página
 	task := chromedp.Tasks{
 		chromedp.Navigate(url),
-		chromedp.WaitVisible("input[type=password]", chromedp.ByQuery),
-		chromedp.SendKeys("input[name=_username]", user, chromedp.ByQuery),
-		chromedp.SendKeys("input[name=_password]", password, chromedp.ByQuery),
-		chromedp.Click("button[type=submit]", chromedp.ByQuery),
+		chromedp.WaitVisible("input[name=password]", chromedp.BySearch),
+		chromedp.SendKeys("input[name=user]", user, chromedp.BySearch),
+		chromedp.SendKeys("input[name=password]", password, chromedp.BySearch),
+		chromedp.Click("button[aria-label=Login button]", chromedp.BySearch),
+		chromedp.WaitVisible("body", chromedp.BySearch),
+		chromedp.Navigate("http://10.115.43.118:3008/il/grafana/d/sDmADcSIk/mle-flr?orgId=1&refresh=30s"),
 		chromedp.Sleep(2 * time.Second),
 		chromedp.FullScreenshot(&buf, 90),
 	}
@@ -134,14 +140,23 @@ func flr(ctx context.Context, bot *tb.Bot, m *tb.Message, url string, user strin
 	return nil
 }
 
-func Dafiti(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
+func Tiendas(ctx context.Context, bot *tb.Bot, m *tb.Message, url string, user string, password string) error {
 
 	var buf []byte
 
 	// Navega a la página
 	task := chromedp.Tasks{
 		chromedp.Navigate(url),
-		chromedp.CaptureScreenshot(&buf),
+		chromedp.WaitVisible("input[name=password]", chromedp.BySearch),
+		chromedp.SendKeys("input[name=user]", user, chromedp.BySearch),
+		chromedp.SendKeys("input[name=password]", password, chromedp.BySearch),
+		chromedp.Click(`button[aria-label="Login button"]`, chromedp.BySearch),
+		chromedp.WaitVisible("body", chromedp.BySearch),
+		chromedp.Sleep(1 * time.Second),
+		chromedp.Navigate("http://10.115.43.82:3002/dashboards"),
+		chromedp.Navigate("http://10.115.43.82:3002/d/yPbn4f2Sk/consolidado-v3-remoto?orgId=4&refresh=1m"),
+		chromedp.Sleep(2 * time.Second),
+		chromedp.FullScreenshot(&buf, 90),
 	}
 
 	err := chromedp.Run(ctx, task)
@@ -162,33 +177,33 @@ func Dafiti(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
 	return nil
 }
 
-func SBS(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
+// func SBS(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
 
-	var buf []byte
+// 	var buf []byte
 
-	// Navega a la página
-	task := chromedp.Tasks{
-		chromedp.Navigate(url),
-		chromedp.CaptureScreenshot(&buf),
-	}
+// 	// Navega a la página
+// 	task := chromedp.Tasks{
+// 		chromedp.Navigate(url),
+// 		chromedp.CaptureScreenshot(&buf),
+// 	}
 
-	err := chromedp.Run(ctx, task)
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	err := chromedp.Run(ctx, task)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	// Crear un lector para el buffer de bytes
-	reader := bytes.NewReader(buf)
+// 	// Crear un lector para el buffer de bytes
+// 	reader := bytes.NewReader(buf)
 
-	// Enviar la imagen como un mensaje de foto
-	photo := &tb.Photo{File: tb.FromReader(reader)}
-	_, err = bot.Send(m.Chat, photo)
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	// Enviar la imagen como un mensaje de foto
+// 	photo := &tb.Photo{File: tb.FromReader(reader)}
+// 	_, err = bot.Send(m.Chat, photo)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 // func flr_info(ctx context.Context, bot *tb.Bot, m *tb.Message, url string, user string, password string) error {
 
