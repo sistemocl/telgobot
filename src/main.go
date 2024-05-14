62d93299c5a200022530152e83c25f1127750212
Nicolas <nicolas.bouffanais.1999@gmail.com>
2024-05-14 18:20:01 -0400

    response with tag username and screenshot, new feature in development:delay 15 seconds between each command

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/chromedp/chromedp"
@@ -14,41 +15,27 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+var (
+	lastCommandTime map[int]time.Time
+	lastCommandLock sync.Mutex
+)
+
 func main() {
+	lastCommandTime = make(map[int]time.Time)
 
 	// Cargar variables de entorno
 	godotenv.Load()
 	token := os.Getenv("BOT_TOKEN")
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
+	admin_user := os.Getenv("ADMIN_USER")
+	admin_pass := os.Getenv("ADMIN_PASS")
 
 	// Verificar si las variables de entorno están definidas
 	if token == "" || user == "" || password == "" {
 		fmt.Println("No se han definido las tres variables de entorno: TOKEN, USER y PASSWORD")
 		return
 	}
-
-	// Utilizar las variables de entorno
-	// fmt.Println("TOKEN:", token)
-	// fmt.Println("USER:", user)
-	// fmt.Println("PASSWORD:", password)
-
-	// Crea un contexto padre para el navegador
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
-	// Crea dos contextos hijo para dos pestañas
-	ctx1, cancel1 := chromedp.NewContext(ctx)
-	defer cancel1()
-
-	ctx2, cancel2 := chromedp.NewContext(ctx)
-	defer cancel2()
-
-	ctx3, cancel3 := chromedp.NewContext(ctx)
-	defer cancel3()
-
-	// ctx4, cancel4 := chromedp.NewContext(ctx)
-	// defer cancel4()
 
 	bot, err := tb.NewBot(tb.Settings{
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
@@ -58,15 +45,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Mapa para almacenar los comandos y sus descripciones
+	// Crear un contexto padre
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
 	commands := map[string]string{
-		"/Dafiti":  "Abre el sitio web de Dafiti.",
-		"/SBS":     "Abre el sitio web de SBS.",
-		"/flr":     "Abre el sitio de FLR.",
-		"flr_info": "Retorna información delicada del sitio",
+		"/tiendas": "Abre el sitio web de Tiendas.",
+		// "/SBS":      "Abre el sitio web de SBS.",
+		//"/Dafiti": "Abre el sitio web de Dafiti.",
+		"/flr": "Abre el sitio de FLR.",
 	}
 
-	// Manejador de comando para mostrar todos los comandos disponibles
 	bot.Handle("/comandos", func(m *tb.Message) {
 		var reply strings.Builder
 		reply.WriteString("Comandos disponibles:\n")
@@ -79,37 +68,110 @@ func main() {
 		bot.Send(m.Chat, reply.String())
 	})
 
-	bot.Handle("/flr", func(m *tb.Message) {
-		go flr(ctx1, bot, m, "https://www.chess.com/login", user, password)
+	bot.Handle("/tiendas", func(m *tb.Message) {
+		command := strings.ToLower(strings.ReplaceAll(m.Text, " ", ""))
+		if command == "/tiendas" {
+			if canExecuteCommand(int(m.Sender.ID)) {
+				screenshot, err := Tiendas(ctx, "http://10.115.43.82:3002/login", admin_user, admin_pass)
+				if err != nil {
+					log.Printf("Error al tomar captura de pantalla: %v", err)
+					return
+				}
+				Photos_response(screenshot, m, bot)
+			} else {
+				bot.Send(m.Sender, "Por favor espera al menos 15 segundos antes de volver a ejecutar el comando.")
+			}
+		} else {
+			bot.Send(m.Chat, "Comando no reconocido. Por favor, intenta nuevamente.")
+		}
+
 	})
 
-	bot.Handle("/Dafiti", func(m *tb.Message) {
-		go Dafiti(ctx2, bot, m, "https://pkg.go.dev/gopkg.in/telebot.v3@v3.2.1")
+	bot.Handle("/test", func(m *tb.Message) {
+		command := strings.ToLower(strings.ReplaceAll(m.Text, " ", ""))
+		if command == "/test" {
+			if canExecuteCommand(int(m.Sender.ID)) {
+				screenshot, err := Example(ctx, "https://chatgpt.com/")
+				if err != nil {
+					log.Printf("Error al tomar captura de pantalla: %v", err)
+					return
+				}
+				Photos_response(screenshot, m, bot)
+			} else {
+				bot.Send(m.Sender, "Por favor espera al menos 15 segundos antes de volver a ejecutar el comando.")
+			}
+		} else {
+			bot.Send(m.Chat, "Comando no reconocido. Por favor, intenta nuevamente.")
+		}
 	})
 
-	bot.Handle("/SBS", func(m *tb.Message) {
-		go Dafiti(ctx3, bot, m, "https://www.youtube.com/")
+	bot.Handle("/test2", func(m *tb.Message) {
+		command := strings.ToLower(strings.ReplaceAll(m.Text, " ", ""))
+		if command == "/test2" {
+			if canExecuteCommand(int(m.Sender.ID)) {
+				screenshot, err := Example(ctx, "https://www.perplexity.ai/")
+				if err != nil {
+					log.Printf("Error al tomar captura de pantalla: %v", err)
+					return
+				}
+				Photos_response(screenshot, m, bot)
+			} else {
+				bot.Send(m.Sender, "Por favor espera al menos 15 segundos antes de volver a ejecutar el comando.")
+			}
+		} else {
+			bot.Send(m.Chat, "Comando no reconocido. Por favor, intenta nuevamente.")
+		}
 	})
 
-	// bot.Handle("/flr_info", func(m *tb.Message) {
-	// 	go flr_info(ctx4, bot, m, "https://www.chess.com/login", user, password)
-	// })
+	go bot.Start()
 
-	log.Println("Chat Bot is running. Press CTRL+C to exit.")
-	bot.Start()
+	select {}
 }
 
-func flr(ctx context.Context, bot *tb.Bot, m *tb.Message, url string, user string, password string) error {
+func canExecuteCommand(userID int) bool {
+	lastCommandLock.Lock()
+	defer lastCommandLock.Unlock()
 
+	lastTime, ok := lastCommandTime[userID]
+	if !ok {
+		lastCommandTime[userID] = time.Now()
+		return true
+	}
+
+	elapsed := time.Since(lastTime)
+	if elapsed >= 20*time.Second {
+		lastCommandTime[userID] = time.Now()
+		return true
+	}
+	return false
+}
+
+func Photos_response(screenshot []byte, m *tb.Message, bot *tb.Bot) {
+	photo := &tb.Photo{
+		File: tb.FromReader(bytes.NewReader(screenshot)),
+	}
+	mention := ""
+	if m.Sender.Username != "" {
+		mention = "@" + m.Sender.Username
+	} else {
+		mention = m.Sender.FirstName
+	}
+	bot.Send(m.Chat, mention+" Aqui tienes:")
+	bot.SendAlbum(m.Chat, tb.Album{photo})
+}
+
+func FLR(ctx context.Context, url, user, password string) ([]byte, error) {
 	var buf []byte
 
 	// Navega a la página
 	task := chromedp.Tasks{
-		chromedp.Navigate(url),
-		chromedp.WaitVisible("input[type=password]", chromedp.ByQuery),
-		chromedp.SendKeys("input[name=_username]", user, chromedp.ByQuery),
-		chromedp.SendKeys("input[name=_password]", password, chromedp.ByQuery),
-		chromedp.Click("button[type=submit]", chromedp.ByQuery),
+		chromedp.Navigate("http://10.115.43.118:3008/il/grafana/login"),
+		chromedp.WaitVisible("input[name=password]", chromedp.BySearch),
+		chromedp.SendKeys("input[name=user]", user, chromedp.BySearch),
+		chromedp.SendKeys("input[name=password]", password, chromedp.BySearch),
+		chromedp.Click("button[aria-label=Login button]", chromedp.BySearch),
+		chromedp.WaitVisible("body", chromedp.BySearch),
+		chromedp.Navigate("http://10.115.43.118:3008/il/grafana/d/sDmADcSIk/mle-flr?orgId=1&refresh=30s"),
 		chromedp.Sleep(2 * time.Second),
 		chromedp.FullScreenshot(&buf, 90),
 	}
@@ -119,29 +181,24 @@ func flr(ctx context.Context, bot *tb.Bot, m *tb.Message, url string, user strin
 		log.Fatal(err)
 	}
 
-	// Crear un lector para el buffer de bytes
-	reader := bytes.NewReader(buf)
-
-	//msg := "@" + m.Sender.Username
-
-	// Enviar la imagen como un mensaje de foto
-	photo := &tb.Photo{File: tb.FromReader(reader)}
-	_, err = bot.Send(m.Chat, photo)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return nil
+	return buf, nil
 }
 
-func Dafiti(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
-
+func Tiendas(ctx context.Context, url, user, password string) ([]byte, error) {
 	var buf []byte
 
-	// Navega a la página
 	task := chromedp.Tasks{
 		chromedp.Navigate(url),
-		chromedp.CaptureScreenshot(&buf),
+		chromedp.WaitVisible("input[name=password]", chromedp.BySearch),
+		chromedp.SendKeys("input[name=user]", user, chromedp.BySearch),
+		chromedp.SendKeys("input[name=password]", password, chromedp.BySearch),
+		chromedp.Click(`button[aria-label="Login button"]`, chromedp.BySearch),
+		chromedp.WaitVisible("body", chromedp.BySearch),
+		chromedp.Sleep(1 * time.Second),
+		chromedp.Navigate("http://10.115.43.82:3002/dashboards"),
+		chromedp.Navigate("http://10.115.43.82:3002/d/yPbn4f2Sk/consolidado-v3-remoto?orgId=4&refresh=1m"),
+		chromedp.Sleep(2 * time.Second),
+		chromedp.FullScreenshot(&buf, 90),
 	}
 
 	err := chromedp.Run(ctx, task)
@@ -149,27 +206,16 @@ func Dafiti(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
 		log.Fatal(err)
 	}
 
-	// Crear un lector para el buffer de bytes
-	reader := bytes.NewReader(buf)
-
-	// Enviar la imagen como un mensaje de foto
-	photo := &tb.Photo{File: tb.FromReader(reader)}
-	_, err = bot.Send(m.Chat, photo)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return nil
+	return buf, nil
 }
 
-func SBS(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
-
+func Example(ctx context.Context, url string) ([]byte, error) {
 	var buf []byte
 
-	// Navega a la página
 	task := chromedp.Tasks{
 		chromedp.Navigate(url),
-		chromedp.CaptureScreenshot(&buf),
+		chromedp.Sleep(2 * time.Second),
+		chromedp.FullScreenshot(&buf, 90),
 	}
 
 	err := chromedp.Run(ctx, task)
@@ -177,39 +223,5 @@ func SBS(ctx context.Context, bot *tb.Bot, m *tb.Message, url string) error {
 		log.Fatal(err)
 	}
 
-	// Crear un lector para el buffer de bytes
-	reader := bytes.NewReader(buf)
-
-	// Enviar la imagen como un mensaje de foto
-	photo := &tb.Photo{File: tb.FromReader(reader)}
-	_, err = bot.Send(m.Chat, photo)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return nil
+	return buf, nil
 }
-
-// func flr_info(ctx context.Context, bot *tb.Bot, m *tb.Message, url string, user string, password string) error {
-
-// 	// Navega a la página
-// 	task := chromedp.Tasks{
-// 		chromedp.Navigate(url),
-// 		chromedp.WaitVisible("input[type=password]", chromedp.ByQuery),
-// 		chromedp.SendKeys("input[name=_username]", user, chromedp.ByQuery),
-// 		chromedp.SendKeys("input[name=_password]", password, chromedp.ByQuery),
-// 		chromedp.Click("button[type=submit]", chromedp.ByQuery),
-// 		chromedp.Sleep(2 * time.Second),
-// 	}
-
-// 	err := chromedp.Run(ctx, task)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	msg := "@" + m.Sender.Username
-
-// 	bot.Send(m.Chat, msg)
-
-// 	return nil
-// }
